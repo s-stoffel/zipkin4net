e0211e74f6e26f10e9399721a1feb82b418aac34
Simon Stoffel <s.stoffel@traveltrex.com>
2019-08-26 10:07:05 +0200

    add test showing that client and server spans get mixed up when the server calls one of its own endpoints (i.e. both is happening in the same process)

--- Src/zipkin4net.middleware.owin/Tests/Helpers/OwinHelper.cs
+++ Src/zipkin4net.middleware.owin/Tests/Helpers/OwinHelper.cs
@@ -9,29 +9,43 @@ namespace zipkin4net.Middleware.Tests.Helpers
 {
     static class OwinHelper
     {
-        internal static async Task<string> Call(Action<IAppBuilder> startup, Func<HttpClient, Task<string>> clientCall)
+        internal static async Task<string> Call(Action<IAppBuilder> startup, Func<HttpClient, Task<string>> clientCall, Action<HttpMessageHandler> rememberTestHttpMessageHandler = null)
         {
             using (var server = TestServer.Create(startup))
             {
+                rememberTestHttpMessageHandler?.Invoke(server.Handler);
                 using (var client = new HttpClient(server.Handler))
                 {
                     return await clientCall(client);
                 }
             }
         }
-        internal static Action<IAppBuilder> DefaultStartup(string serviceName, Func<IOwinContext, string> getRpc = null, Func<PathString, bool> routeFilter = null)
+        internal static Action<IAppBuilder> DefaultStartup(string serviceName, Func<IOwinContext, string> getRpc = null, Func<PathString, bool> routeFilter = null, Func<IOwinContext, Task> runHandler = null)
         {
             return
                 app =>
                 {
                     app.UseZipkinTracer(serviceName, getRpc, routeFilter);
 
-                    app.Run(async context =>
+                    if (runHandler == null)
                     {
-                        context.Response.ContentType = "text/plain";
-                        await context.Response.WriteAsync(DateTime.Now.ToString());
-                    });
+                        runHandler = async context =>
+                        {
+                            context.Response.ContentType = "text/plain";
+                            await context.Response.WriteAsync(DateTime.Now.ToString());
+                        };
+                    }
+                    app.Run(runHandler);
                 };
+        }
+
+        internal static Func<HttpClient, Task<string>> ClientCall(Uri urlToCall)
+        {
+            return async client =>
+            {
+                var response = await client.GetAsync(urlToCall);
+                return await response.Content.ReadAsStringAsync();
+            };
         }
     }
 }
